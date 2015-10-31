3af0c19cb037f5726bc21a946884d54cb1353ce7
WillJHaggard <willjhaggard@gmail.com>
2015-10-31 17:00:21 -0400

    shorthand-variable.js script functionality

---
+++ js/shorthand-variable.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+    var price = 5,
+        quantity = 14,
+        total = price * quantity;
+
+    var el = document.getElementById('cost');
+    el.textContent = '$' + total;
+
+
+
+
+
+
+}) ();
