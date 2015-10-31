e1db617a3217fee7bbfa8db59bf87627be0db1ce
WillJHaggard <willjhaggard@gmail.com>
2015-10-31 14:50:32 -0400

    Added string-variable.js script functionality

---
+++ js/string-variable.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+    // Create vars to hold the name and note text
+    var username,
+        message;
+
+        // Assign
+        username = 'Will';
+        message = 'See our upcoming range';
+
+        // Get the element with an id of name
+        var elName = document.getElementById('name');
+        // Replace the content of this element.
+        elName.textContent = username;
+
+        // Get the element with an id of note.
+        var elNote = document.getElementById('note');
+        // Replace the content of this element.
+        elNote.textContent = message;
+
+
+
+
+
+}) ();
