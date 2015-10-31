858ae406bf69a098391664eff54bb3a90a7d2512
WillJHaggard <willjhaggard@gmail.com>
2015-10-31 17:22:25 -0400

    arithmetic-operator.js script functionality

---
+++ js/arithmetic-operator.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+    //Create a var for the subtotal and make a calculation
+    var subtotal = (13 + 1) * 5; // Subtotal is 70
+
+    //Create a variable for the shipping and make a calculation
+    var shipping = 0.5 * (13 + 1);
+
+    //Create the total by combining the subtotal and shipping values
+    var total = subtotal + shipping;
+
+    //Write the results to the screen
+    var elSub = document.getElementById('subtotal');
+    elSub.textContent = subtotal;
+
+    var elShip = document.getElementById('shipping');
+    elShip.textContent = shipping;
+
+    var elTotal = document.getElementById('total');
+    elTotal.textContent = total;
+}) ();
