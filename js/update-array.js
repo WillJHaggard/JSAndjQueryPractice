fb2110c913e707734b0072fcbfb3784c7a2606a2
WillJHaggard <willjhaggard@gmail.com>
2015-10-31 17:12:56 -0400

    update-array.js script functionality

---
+++ js/update-array.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+    // Create the array and assign it values
+    var colors = ['white', 'black', 'custom'];
+
+    // Update the third item in the array
+    colors[2] = 'beige';
+
+    // Get the element with an id of colors
+    var el = document.getElementById('colors');
+    // Replace element with third item from the array
+    el.textContent = colors[2];
+
+
+
+}) ();
